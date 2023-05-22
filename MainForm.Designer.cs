2a43ecda6f1e2f295f0db47099a9bf61da0ccdfa
TruePelmen <oleksiisenyshyn@lnu.edu.ua>
2023-05-22 20:32:12 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Game
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.labelLose = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.AllowDrop = true;
+            this.buttonPlay.Location = new System.Drawing.Point(271, 136);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(141, 66);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonExit.Location = new System.Drawing.Point(432, 136);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(141, 66);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(93, 44);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(61, 20);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Score: 0";
+            // 
+            // labelLose
+            // 
+            this.labelLose.AutoSize = true;
+            this.labelLose.Location = new System.Drawing.Point(380, 91);
+            this.labelLose.Name = "labelLose";
+            this.labelLose.Size = new System.Drawing.Size(80, 20);
+            this.labelLose.TabIndex = 3;
+            this.labelLose.Text = "You lost =(";
+            // 
+            // ball
+            // 
+            this.ball.Image = global::Game.Properties.Resources.Red_Ball_PNG;
+            this.ball.Location = new System.Drawing.Point(476, 91);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(46, 37);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 4;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(846, 484);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.labelLose);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label labelLose;
+        private System.Windows.Forms.PictureBox ball;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class MainForm : Form
+    {
+        int score = 5;
+        public void Game()
+        {
+            buttonExit.Enabled = true;
+            buttonExit.Visible = false;
+            buttonPlay.Enabled = true;
+            buttonPlay.Visible = false;
+            scoreLabel.Visible = true;
+            ball.Visible = true;
+            score = 5;
+            Random rand = new Random();
+            ball.Top = rand.Next(100, 400);
+            timer.Enabled = true;
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            labelLose.Visible = false;
+            scoreLabel.Visible = false;
+            ball.Visible = false;
+            scoreLabel.Text = $"Score: {score}";
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            labelLose.Visible = false;
+            Game();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(score<0)
+            {
+                labelLose.Visible = true;
+                ball.Visible = false;
+                timer.Enabled = false;            
+                buttonExit.Enabled = true;
+                buttonExit.Visible = true;
+                buttonPlay.Enabled = true;
+                buttonPlay.Visible = true;
+                return;
+            }     
+
+            Random rand = new Random();
+            ball.Top = rand.Next(100, 400);
+            ball.Left = rand.Next(100, 600);
+            score -= 1;
+            scoreLabel.Text = $"Score: {score}";
+
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            ball.Top = rand.Next(100, 400);
+            ball.Left = rand.Next(100, 600);
+            score += 1;
+            scoreLabel.Text = $"Score: {score}";
+            timer.Enabled = false;
+            timer.Enabled = true;
+
+        }
+        //private void buttonExit(object sender, EventArgs e)
+        //{
+        //    Close();
+        //}
+
+    }
+}
